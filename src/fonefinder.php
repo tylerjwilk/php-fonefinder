fca8b9b245fae60b9dbf7724f41742c154805b03
Tyler Wilkinson <tylerjwilk@gmail.com>
2013-02-19 03:40:57 -0500

    Added the file structure.

---
+++ src/fonefinder.php
@@ -0,0 +1,30 @@
+<?php
+
+    #doc
+    #    classname:    FoneFinder
+    #    scope:        PUBLIC
+    # description:     Reverse cell carrier lookups to convert cell phone numbers to carrier names and gateways.
+    #
+    #/doc
+    
+    class FoneFinder
+    {
+        /*
+         * Class variables
+         */
+        private $npa = false;
+        private $nxx = false;
+        private $thoublock = false;
+    
+        /*
+         * Constructor
+         */
+        function __construct (int $npa, int $nxx, int $thoublock)
+        {
+            // TODO
+            
+        }
+    
+    
+    }
+  
